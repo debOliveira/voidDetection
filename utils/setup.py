d1c93e71b756a896c2b02f818a6555d3b9ac0497
Débora Oliveira <48807586+dbOlv@users.noreply.github.com>
2021-03-12 09:10:03 -0300

    Removing unknown character

--- utils/setup.py
+++ utils/setup.py
@@ -14,7 +14,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import parameters
-import functions
+import functions
 
 if (cv. __version__ != "4.4.0"):
   print("Please install cv2 version 4.4.0")
